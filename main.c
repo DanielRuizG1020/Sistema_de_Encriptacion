778ebfe2dc2c01f13b6bca902cbf19e05602a32f
DanielRuizG1020 <142362165+DanielRuizG1020@users.noreply.github.com>
2024-03-15 19:05:13 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,9 @@
 
 /*
-/brief este es el primer programa pa probar el compilador y el sistema de desarrollo de git hub
-/author Daniel Ruiz Guirales, Andres Felipe Pega Betancur
+/brief Algoritmos implementados en el sistema de encriptacion
+/author Daniel Ruiz Guirales, Andres Felipe Penagos Betancur
 */
 //Librerias
-
-
-//falta prototipo de la funcion
 
 #include <stdio.h>
 #include <stdint.h>
